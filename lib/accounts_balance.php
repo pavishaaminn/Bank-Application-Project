a63059dcb83780c52d6854c6bd753135d79736fc
pa284 <pa284@njit.edu>
2022-12-21 17:29:30 -0500

    changes to deposit/withdrawal

--- lib/accounts_balance.php
+++ lib/accounts_balance.php
@@ -2,13 +2,13 @@
 function accounts_balance($account_id){
     if(is_logged_in(true)){
         $query = "UPDATE Accounts set balance = (SELECT IFNULL(SUM(BalanceChange),0)
-        from Transactions WHERE AccountSrc = :accountsrc) WHERE id = :accountsrc";
+        from Transactions WHERE AccountSrc = :accountsrc WHERE id = :accountsrc";
         $db = getDB();
         $stmt = $db -> prepare($query);
         try{
             $stmt->execute([":accountsrc" => $account_id]);
         } catch (PDOException $e){
-            flash("Error");
+            error_log(var_export($e, true));
         }
     }
 }
